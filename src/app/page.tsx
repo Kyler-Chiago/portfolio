d9209f33ca87b38c70bde6abc81977bdb49dccea
Kyler-Chiago <kchiago50@gmail.com>
2025-03-17 04:27:06 -0700

    intro and header progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,121 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className='wrapper'>
+      <div className='header'>
+        <div className='headerLeft text-lg font-semibold'>Kyler Chiago</div>
+        <div className='headerRight text-base'>
+          <div className='headerAbout'>About</div>
+          <div className='headerProjects ml-[20px]'>Projects</div>
+          <div className='headerLM ml-[20px]'>
+            <a
+              onClick = {() => {console.log('Hello')}}
+            >
+              <Image
+                className='icon'
+                src={'/lightModeButton_final.png'}
+                width={32}
+                height={32}
+                alt='G'
+              />
+            </a>
+            {/* <a
+              href={'https://www.linkedin.com/in/kyler-chiago/'}
+              className='teamCardLink2'
+            >
+              <Image
+                className='icon'
+                src={'/LinkedInLogo.png'}
+                width={25}
+                height={25}
+                alt='L'
+              />
+            </a> */}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className='bodyWrapper'>
+        <div className='intro'>
+          <div className="pfpDiv">
+            <Image
+              className='pfp'
+              src={'/pfp.png'}
+              width={300}
+              height={500}
+              fill={false}
+              sizes='{min-width: 300px}'
+              alt='L'
+            />
+          </div>
+          <div className='introText'>
+            <div className="text-6xl font-semibold">Hello, I&apos;m Kyler!</div>
+            <div className="text-lg mt-[16px]">
+              I&apos;m a Software Engineer currently based in Phoenix, AZ and I
+              build software to make life easier and more meaningful.
+            </div>
+            <div className='links mt-[16px]'>
+              <a
+                href={'https://github.com/Kyler-Chiago'}
+                className='teamCardLink1'
+              >
+                <Image
+                  className=''
+                  src={'/GithubLogo.png'}
+                  width={32}
+                  height={32}
+                  alt='G'
+                />
+              </a>
+              <a
+                href={'https://www.linkedin.com/in/kyler-chiago/'}
+                className='teamCardLink2 ml-[16px]'
+              >
+                <Image
+                  className=''
+                  src={'/LinkedInLogo.png'}
+                  width={32}
+                  height={32}
+                  alt='L'
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div>Arrow Down</div>
+        <div className='bio'>About Me</div>
+        <div>dash</div>
+        <div className='bioWrapper'>
+          <div className='bioLeft'>
+            Hi, I&apos;m Ashley and I am a highly ambitious, self-motivated, and
+            driven software engineer based in Los Angeles, CA. My journey began
+            at California State University, Long Beach, where I earned a BA in
+            History. I successfully completed a Coding Advanced Immersive
+            Program, paving the way for my transition into the tech sphere.
+            I&apos;m adept at quickly learning new technologies and have
+            experience in building web applications to create efficient,
+            expansible user-curated experiences. Beyond code and algorithms, I
+            find joy in a spectrum of hobbies that enrich my life. Whether
+            strumming melodies on my guitar, delving into the latest tech
+            trends, or embarking on travel adventures, I am a firm believer in
+            embracing diverse experiences and fostering continuous personal and
+            professional growth. I am committed to unraveling my boundless
+            potential and passion within the dynamic tech realm. I am excited to
+            see where my career takes me and am always open to new
+            opportunities. 🙂
+          </div>
+          <div className='bioRight'>
+            <div className='mySkills'>My Skills</div>
+            <div className='skillsBoxes'>Box</div>
+          </div>
+        </div>
+        <div className='projects'>projects</div>
+        <div>dash</div>
+        <div className='projectsWrapper'>Box</div>
+      </div>
+      <div className='footer'>footer</div>
     </div>
   );
 }
